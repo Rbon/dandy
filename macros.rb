3777ed141b155768860d6a1eca9469c75560b79b
Robin Bisho <robinbisho@gmail.com>
2016-02-23 21:12:48 -0800

    Improved check method, and renamed it to get_phrase. Still needs work.

--- macros.rb
+++ macros.rb
@@ -1,5 +1,7 @@
 ##
-# TODO: support for varying string length for arrays searched using get_name
+# TODO: Support for varying string length for arrays searched using get_phrase.
+#       Logging.
+#       Hard-coded commands that don't require a creature as first phrase.
 
 require "json"
 
@@ -33,7 +35,8 @@ class Macros
     return sum, dice
   end
   
-  # Rolls to hit, i.e. 1d20 + ability mod.
+  # Rolls to hit, i.e. 1d20 + attack mod.
+  #   Automatically re-rolls for crits and fumbles.
   #
   # attack_mod - An Integer of the total attack modifier for the action being
   #              performed.
@@ -43,20 +46,35 @@ class Macros
   #   hit(2)
   #   # => 16 to hit [14 + 2]
   #
-  # Returns a String detailing how the roll went.
+  # Returns a multiple-line String detailing how the roll went.
   def self.hit(attack_mod)
-    result = rand(1..20)
-    return "#{result + attack_mod} to hit ([#{result}] + #{attack_mod})"
+    result = ""
+    roll = rand(1..20)
+    result += "#{roll + attack_mod} to hit | [#{roll}] + #{attack_mod}"
+    
+    if roll == 20
+      result += "\nRolling to confirm crit...\n"
+      roll = rand(1..20)
+      result += "#{roll + attack_mod} to hit | [#{roll}] + #{attack_mod}"
+      
+    elsif roll == 1
+      result += "\nRolling to confirm fumble...\n"
+      roll = rand(1..20)
+      result += "#{roll + attack_mod} to hit | [#{roll}] + #{attack_mod}"
+    end
+      
+    return result
     
     # if result == 20
     #   crit_roll = hit
     # end
   end
   
-  def self.attack(action)
-    result = roll(action[1])
-    return "#{hit(action[0])}\n"\
-           "#{result[0]} #{action[2]} damage #{result[1]}"
+  def self.macro(creature, action)
+    result = roll(action.info[1])
+    return "#{creature.name} uses #{action.name}\n"\
+           "#{hit(action.info[0])}\n"\
+           "#{result[0]} #{action.info[2]} damage | #{result[1]}"
   end
 end
 
@@ -66,7 +84,7 @@ class Creature
   attr_reader :str_mod, :dex_mod, :con_mod, :int_mod, :wis_mod, :cha_mod,
               :actions, :name
               
-  # Initialize a Creature
+  # Initialize a Creature.
   #
   # name - A String that will be displayed as part of actions rolled.
   # info - An Array of Strings denoting ability scores, actions, and other
@@ -85,7 +103,11 @@ class Creature
     @int_mod = mod(@int)
     @wis_mod = mod(@wis)
     @cha_mod = mod(@cha)
-    @actions = actions
+    @actions = { }
+    
+    info["actions"].each do |name, info|
+      @actions[name.downcase] = Action.new(name, info)
+    end
   end
   
   # Get an ability modifier.
@@ -103,6 +125,15 @@ class Creature
   end
 end
 
+# An action that a Creature can take.
+class Action
+  attr_reader :name, :info
+  def initialize(name, info)
+    @name = name
+    @info = info
+  end
+end
+
 # The class which contains the main loop, as well as other methods nessicary
 #   for running the script.
 class Main
@@ -112,7 +143,7 @@ class Main
       file_hash = JSON.parse(file)
       
       file_hash.each do |name, info|
-        @creatures[name] = Creature.new(name, info)
+        @creatures[name.downcase] = Creature.new(name, info)
       end
     end
   
@@ -123,21 +154,28 @@ class Main
       
       if command == "quit"
         exit
+        
       else
-        puts check(command, @creatures.keys)
-        # creature = check(command, @creatures.keys)
-        # action   = check(creature[1], creature[2]])
-        # @creatures[creature].actions
-         
+        first_pass = get_phrase(command, @creatures.keys)
+        
+        if first_pass.class == Array
+          creature = @creatures[first_pass[0]]
+          second_pass = get_phrase(first_pass[1], creature.actions.keys)
+          
+          if second_pass.class == Array
+            action = creature.actions[second_pass[0]]
+            puts Macros.macro(creature, action) + "\n\n"
+          end
+        end
       end
     end
   end
   
   # Determine if a String begins with any item in an Array of Strings.
-  #   Auto-complete words.
+  #   Auto-complete words. Does not ignore case.
   #
-  # command - The String that will be parsed.
-  # names   - The Array of Strings that the command will be checked against.
+  # command - A String that will be parsed.
+  # phrases - An Array of Strings that the command will be checked against.
   # depth   - An Integer of how many words at the start of the command should be
   #           checked. This number is automatically incremeneted as the method
   #           recurses, and should not be set manually.
@@ -150,33 +188,34 @@ class Main
   #   check("kni roll 1d6", ["knight", "priest"])
   #   # => ["knight", "roll 1d6"]
   #
-  # Returns the matching item, and the remainder of command, but only if one
-  #   match is found. Returns "NO MATCH" for 0 matches or "INCONCLUSIVE" for
-  #   any number of matches more than 1.
-  def get_name(command, names, depth = 0)
-    word = command.split[0]
+  # Returns an Array containing the matching item, and the remainder of command
+  #   if one match is found.
+  #   Returns "NO MATCH" for 0 matches.
+  #   Returns "TOO MANY MATCHES" for any number of matches greater than 1.
+  def get_phrase(command, phrases, depth = 0)
+    word = command.split[depth]
     matches = []
   
-    names.each do |name|
-      if name.split[depth..-1].join(" ").start_with?(word)
-        matches << name
+    phrases.each do |phrase|
+      if phrase.split[depth..-1].join(" ").start_with?(word)
+        matches << phrase
       end
     end
     
     if matches.length == 0
       return "NO MATCH"
+      
     elsif matches.length == 1
-      return matches * " ", command.sub(word, "").lstrip
+      return matches * " ", command.split[depth + 1..-1].join(" ")
+      
     else
       if command.split.length == 1 # avoids infinite recursion
-        return "INCONCLUSIVE"
+        return "TOO MANY MATCHES"
       else
-        return get_name(command.sub(word, ""), matches, depth + 1)
+        return get_phrase(command, matches, depth + 1)
       end
     end
   end
 end
 
-
-# puts Macros.hit(3)
 Main.new.run
