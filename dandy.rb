30f45a72039f4264db8b2a41d412b09024e18ce4
Robin Bisho <robinbisho@gmail.com>
2017-04-01 05:30:47 -0700

    Fixed a corrupted tree object and am too lazy to redo the branch, so this commit's a doozy.

--- dandy.rb
+++ dandy.rb
@@ -1,288 +1,300 @@
-# TODO:
-#   Write a better description for get_phrase.
-#   "Again" command.
-#   Spells.
+## TODO:
+## Spells.
+##
+## Proper help text.
+##
+## Smarter tab completion.
 
-# BUGS:
-#   Commas sometimes fuck with get_phrase.
+## BUGS:
+## Un-numlock'd numberpad keys still insert text into the line editor.
+##
+## Backspace acts differently on other computers.
+##
+## Syntax errors in sub-commands compound with the same errors in the
+## super-command.
 
 require "json"
+require "curses"
 
-# Methods that allow for dice rolls, with common roll types having their own
-#   methods. All methods are module methods and should be called on the Macros
-#   module.
-class Macros
-  # Makes a speudorandom dice roll of any arbitrary count, faces, and modifiers.
-  #
-  # notation - the dice notation, as a String.
-  #
-  # Examples
-  #
-  #   roll("4d8 + 2")
-  #   # => [16, "| [4, 3, 1, 6] + 2"]
-  #
-  # Returns an Array with two items--an Integer sum of the rolls, and a nicely
-  #   formatted String of individual rolls, and the modifier, if there was one.
-  def self.roll_dice(notation)
-    sum = 0
-    operator = nil
-    mod = nil
-    count, faces = notation.delete(" ").split("d")
-    rolls = []
-    
-    ["+","-"].each do |symbol|
-      if faces.include?(symbol)
-        faces, mod = faces.split(symbol)
-        operator = symbol
-      end
-    end
-    
-    (1..count.to_i).each do
-      roll = rand(1..faces.to_i)
-      rolls << roll
-      sum  += roll
-    end
-    
-    rolls = "| #{rolls}"
-    
-    case operator
-    when "+"
-      sum += mod.to_i
-      rolls += " + #{mod}"
-    when "-"
-      sum -= mod.to_i
-      rolls += " - #{mod}"
-    end
-    
-    return sum, rolls
-  end
-  
-  # Rolls to hit, i.e. 1d20 + attack mod.
-  #   Automatically re-rolls for crits and fumbles.
-  #
-  # attack_mod - An Integer of the total attack modifier for the action being
-  #              performed.
-  #
-  # Example
-  #
-  #   hit(2)
-  #   # => 16 to hit [14 + 2]
-  #
-  # Returns a multiple-line String detailing how the roll went.
-  def self.hit(attack_mod, advantage = nil)
-    result = ""
-    roll = roll_dice("1d20 + #{attack_mod}")
-    result += "#{roll[0]} to hit #{roll[1]}"
-    
-    if roll[0] == 20 + attack_mod
-      result += "\nRolling to confirm crit...\n"
-      result += hit(attack_mod)
-      
-    elsif roll[0] == 1 + attack_mod
-      result += "\nRolling to confirm fumble...\n"
-      result += hit(attack_mod)
-    end
-    
-    if advantage
-      advantage = Main.get_phrase(advantage, ["advantage", "disadvantage"])[0]
-      case advantage
-      when "advantage"
-        result += "\nRolling for advantage...\n"
-        result += hit(attack_mod)
-      when "disadvantage"
-        result += "\nRolling for disadvantage...\n"
-        result += hit(attack_mod)
-      end
-    end
-    
-    
-    return result
-  end
-  
-  def self.user_roll(notation)
-    if /\d+d[\df%]+(?:\s*[+-]\s*\d+)?/.match(notation)
-      roll = Macros.roll_dice(notation)
-      return "#{roll[0]} #{roll[1]}"
-    else
-      return "BAD NOTATION"
-    end
-  end
-end
-
-# A character sheet, basically. Automatically creates lambdas for every action
-#   the creature can take, derived from the macros file.
-class Creature
-  attr_reader :macros
-  
-  # Initializes a Creature.
-  #
-  # name - A String that will be set as the creature's name, and displayed as
-  #        part of actions rolled.
-  # info - An Array of ability scores, actions, and other nessisary data.
-  def initialize(name, info)
-    @name   = name
-    @macros = { }
-    
-    info["actions"].each do |name, info|
-      @macros[name.downcase] = lambda do |advantage = nil|
-        roll = Macros.roll_dice(info[1])
-        return "#{@name} uses #{name}\n"\
-          "#{Macros.hit(info[0], advantage)}\n"\
-          "#{roll[0]} #{info[2]} damage #{roll[1]}"
-      end
-    end
-    
-    info.each do |key, value|
-      case key
-      when "str", "dex", "con", "int", "wis", "cha"
-        @macros["roll " + key] = lambda do |junk|
-          roll = Macros.roll_dice("1d20 + #{(value - 10) / 2}")
-          return "#{@name} rolls #{key.upcase}\n"\
-            "#{roll[0]} #{roll[1]}"
-        end
-      end
-    end
-    
-    @macros["roll initiative"] = lambda do |junk|
-      roll = Macros.roll_dice("1d20 + #{(info["dex"] - 10) / 2}")
-      return "#{@name} rolls Initiative\n"\
-        "#{roll[0]} #{roll[1]}"
-    end
-  end
-end
-
-# The class which contains the main loop, as well as other methods nessicary
-#   for running the script.
 class Main
-  def initialize
-      @creatures = { }
-      file = File.read("macros.json")
-      file_hash = JSON.parse(file)
-      
-      file_hash.each do |name, info|
-        @creatures[name.downcase] = Creature.new(name, info)
-      end
-      
-      @macros = {
-        "roll"  => lambda { |notation| Macros.user_roll(notation) },
-        # "again" => lambda { |junk| Macros.again}
-        "quit"  => lambda { |junk| quit(junk) }
-      }
-      
-    end
-    
-  def run
-    while true do
-      print " > "
-      command = gets.rstrip!.downcase
+  attr_reader :aliases
+  attr_writer :running
 
-      action, remainder = Main.get_phrase(command, @macros.keys)
-      action = @macros[action]
-      
-      if action
-        display(action.call(remainder))
-      
-      else
-        creature, remainder = Main.get_phrase(command, @creatures.keys)
-        creature = @creatures[creature]
-        if creature
-          action, remainder = Main.get_phrase(remainder, creature.macros.keys)
-          action = creature.macros[action]
-          if action
-            display(action.call(remainder))
+  def initialize
+    @output_buffer = ""
+    @input_buffer = ""
+    @history = [""]
+    @history_pos = 0
+    @running = true
+    @curs_pos = 0
+    normal_keys =
+        "`1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./'`"\
+        "~!@\#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:\"ZXCVBNM<>? ".split("")
+    @keys = {
+      tab: 9,
+      enter: 10,
+      ctrl_w: 23,
+      down_arrow: 258,
+      up_arrow: 259,
+      backspace: 263,
+      left_arrow: 260,
+      right_arrow: 261,
+      home: 262,
+      delete: 330,
+      end: 360,
+      normal_keys: normal_keys
+    }
+    @commands = ["quit", "exit", "roll", "help"]
+    @aliases = {"attack" => "roll 1d20"}
+    @builtins = Builtins.new(self)
+    @debug = false
+  end
+
+  def run
+    begin
+      Curses.init_screen
+      # Curses.curs_set(0) ## Invisible cursor
+      Curses.noecho ## Don't display pressed characters
+      term_w = Curses.cols - 1
+      term_h = Curses.lines - 1
+      @input_window = Curses::Window.new(1, term_w, term_h, 0)
+      @input_window.keypad = true
+      @output_window = Curses::Window.new(term_h, term_w, 0, 0)
+      @input_window.addstr(" > ")
+
+      ## causes the screen to flicker once at boot so it doesn't flicker again
+      @output_window.refresh
+      @input_window.refresh
+      @silent = true
+      @builtins.exec("config")
+      @silent = false
+      while @running
+        handle_input(@input_window.getch)
+      end
+    rescue Interrupt
+    ensure
+      Curses.close_screen
+    end
+  end
+
+  def handle_input(input)
+    case input
+
+    when @keys[:tab]
+      current_word = @input_buffer.split(" ")[-1]
+      if current_word
+        @commands.each do |arg|
+          if arg.start_with?(current_word)
+            line = "#{@input_buffer.split(" ")[0..-2].join(" ")} #{arg} ".lstrip
+            clear_line
+            @input_window.addstr(line)
+            @input_buffer = String.new(line)
+            @curs_pos = @input_buffer.length
+            @input_window.refresh
           end
         end
       end
-      
-      puts
-    end
-  end
-  
-  def display(text)
-    file = File.new("log.txt", "a")
-    file.write("#{text} \n\n")
-    file.close
-    puts text
-  end
-    
-  # Does more magic than you can comprehend. Shits out the phrase that you
-  #   actually meant to type.
-  #
-  # command - A String that will be parsed.
-  # phrases - An Array of Strings that the command will be checked against.
-  #
-  # Examples
-  #
-  #   check("fo baz", ["foo bar"])
-  #   # => ["foo bar", "baz"]
-  #
-  #   check("d roll 1d6", ["dragon knight", "priest"])
-  #   # => ["dragon knight", "roll 1d6"]
-  #
-  # Returns an Array with two items; either a String of the matched phrase or an
-  #   empty String if no conclusive match is found, and a String of the
-  #   unmatched remainder of the command.
-  def self.get_phrase(command, phrases)
-    result = [""]
-    tmp = []
-    phrases.each do |phrase|
-      tmp.push(phrase.split)
-    end
-  
-    # I have no idea how this line works
-    rotated = Array.new(tmp.map(&:length).max){|i| tmp.map{|e| e[i]}}
-    
-    rotated.each do |phrase|
-      candidate = nil
-      candidate_count = 0
-      
-      phrase.each do |phrase_word|
-        command_word = command.split[rotated.index(phrase)]
-        
-        if phrase_word and command_word
-          if phrase_word.start_with?(command_word)
-            if candidate != phrase_word
-              candidate = phrase_word
-              candidate_count += 1
-            end
-          end
+
+    when @keys[:enter]
+      if @input_buffer.length != 0
+        draw_output(" > #{@input_buffer}")
+        clear_line
+        if @history.include?(@input_buffer)
+          @history.delete(@input_buffer)
         end
+        @history.insert(1, String.new(@input_buffer))
+        draw_output(run_command(@input_buffer).to_s)
+        draw_output(" ")
+        @input_buffer = ""
+        @curs_pos = 0
+        @history_pos = 0
       end
-      
-      if candidate_count == 1
-        result[0] += candidate + " "
+
+    when @keys[:backspace]
+      if @curs_pos > 0
+        @curs_pos -= 1
+        remove_character(@curs_pos)
+        @input_window.refresh
       end
-    end
-    
-    result[0].rstrip!
-    result.push(command.split[result[0].split.length..-1].join(" "))
-    
-    if not phrases.include?(result[0])
-      candidate = nil
-      candidate_count = 0
-      
-      phrases.each do |phrase|
-        if phrase.start_with?(result[0])
-          candidate = phrase
-          candidate_count += 1
-        end
+
+    when @keys[:delete]
+      if @curs_pos < @input_buffer.length
+        remove_character(@curs_pos)
+        @input_window.refresh
+        @input_window.refresh
       end
-      
-      if candidate_count == 1
-        result[0] = candidate
+
+    when @keys[:left_arrow]
+      if @curs_pos > 0
+        @curs_pos -= 1
+        @input_window.setpos(0, @curs_pos + 3)
+      end
+
+    when @keys[:right_arrow]
+      if @curs_pos < @input_buffer.length
+        @curs_pos += 1
+        @input_window.setpos(0, @curs_pos + 3)
+      end
+
+    when @keys[:up_arrow]
+      if @history_pos == @history.length - 1
+        access_history(0)
       else
-        result[0] = ""
-        result[1] = command
+        access_history(@history_pos + 1)
+      end
+
+    when @keys[:down_arrow]
+      if @history_pos == 0
+        access_history(@history.length - 1)
+      else
+        access_history(@history_pos - 1)
+      end
+
+    when @keys[:home]
+      @input_window.setpos(0, 3)
+      @curs_pos = 0
+
+    when @keys[:end]
+      @input_window.setpos(0, 3 + @input_buffer.length)
+      @curs_pos = @input_buffer.length
+
+    else
+      if @keys[:normal_keys].include?(input.to_s)
+        @input_buffer.insert(@curs_pos, input.to_s)
+        @input_window.setpos(0, 3)
+        @input_window.addstr(@input_buffer)
+        @curs_pos += 1
+        @input_window.setpos(0, @curs_pos + 3)
+        @input_window.refresh
+      else
+        draw_output(input.to_s)
       end
     end
-    
-    return result
   end
-  
-  def quit(junk)
-    exit
+
+  def access_history(position)
+    @history_pos = position
+    clear_line
+    @input_window.addstr(@history[@history_pos])
+    @input_buffer = String.new(@history[@history_pos])
+    @curs_pos = @input_buffer.length
   end
-  
+
+  def clear_line
+    @input_window.setpos(0, 3)
+    @input_window.addstr(" " * @input_buffer.length)
+    @input_window.setpos(0, 3)
+  end
+
+  def remove_character(position)
+    @input_buffer.slice!(position)
+    @input_window.setpos(0, 3)
+    @input_window.addstr("#{@input_buffer} ")
+    @input_window.setpos(0, position + 3)
+  end
+
+  def draw_output(string)
+    return if @silent
+    string = string.scan(/.{1,#{@output_window.maxx-1}}/).join("\n")
+    @output_buffer << string
+    @output_buffer = @output_buffer.split("\n")
+    if @output_buffer.length > @output_window.maxy
+      delta = @output_buffer.length - @output_window.maxy
+      @output_buffer = @output_buffer[delta..-1]
+    end
+    if @output_buffer.length < @output_window.maxy
+      delta = @output_window.maxy - @output_buffer.length
+      @output_window.setpos(delta, 0)
+    else
+      @output_window.setpos(0, 0)
+    end
+    @output_buffer = @output_buffer.join("\n") + "\n"
+    @output_window.addstr(@output_buffer)
+    @output_window.refresh
+    @input_window.refresh
+  end
+
+  def debug_output(string)
+    draw_output(string) if @debug
+  end
+
+  def run_command(line)
+    debug_output("run_command: got line: #{line}")
+    @args = line.split(" ", 2)
+    # return run_command(@aliases[@args[0]]) if @aliases[@args[0]]
+    if @aliases[@args[0]]
+      debug_output("run_command: got alias: #{@args[0]}")
+      output = run_command(@aliases[@args[0]])
+    end
+    output =
+      case @args[0]
+      when /^((?:\d+d\d+)(?:[-\+]\d+)?)$/ then @builtins.roll(line)
+      when "alias" then @builtins.alias(@args[1])
+      when "exec" then @builtins.exec(@args[1])
+      when "roll" then @builtins.roll(@args[1])
+      when "echo" then @builtins.echo(@args[1])
+      when "exit", "quit" then @builtins.quit
+      else "No such command: #{@args[0].inspect}"
+      end
+    debug_output("run_command: output: #{output}")
+    return output
+  end
+
+  def expand_line(line)
+    new_line = []
+    line.split(" ").each do |word|
+      if word.match?(/\([^\(\)]*\)/)
+        debug_output("expand_line: got parens match")
+        new_word = run_command(word[1..-2])
+        new_line.push(new_word)
+      else
+        new_line.push(word)
+      end
+    end
+    line = new_line.join(" ")
+  end
+end
+
+class Builtins
+  def initialize(core)
+    @core = core
+  end
+
+  def alias(args)
+    @core.debug_output("alias: got args: #{args}")
+    return "alias: bad syntax" unless args
+    name, command = args.split(" ", 2)
+    @core.aliases[name] = command
+    return "Alias \"#{name}\" set as: #{command}"
+  end
+
+  def echo(args)
+    return @core.expand_line(args)
+  end
+
+  def roll(args)
+    args.match(/^(\d+)d(\d+)([-\+]\d+)?$/) do |notation|
+      results = []
+      count, sides, mod = notation.captures
+      count.to_i.times { results.push(rand(1..sides.to_i)) }
+      notation = "#{count}d#{sides}#{mod.to_s}"
+      return "#{results.sum + mod.to_i} (#{count}d#{sides}#{mod.to_s})"
+    end
+    return "roll: bad syntax"
+  end
+
+  def quit
+    @core.running = false
+  end
+
+  def exec(file)
+    return "exec: no file given" unless file
+    return "exec: no such file #{file}" unless File.exists?(file)
+    @core.draw_output("Executing file: #{file}")
+    File.readlines(file).each { |line| @core.run_command(line) }
+    @core.draw_output("Done")
+  end
 end
 
 Main.new.run
+
